cb0d78ad720dda01cfb6a6cc1676dea810f69ae0
abhishek <abhishek@finemake.in>
2023-08-23 12:27:47 +0530

    setup ORM sqlalchemy

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=G:\FastApi\venv
+set VIRTUAL_ENV=D:\Work_Space\Backend\FastApi\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
